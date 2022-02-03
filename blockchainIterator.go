3ea1b03c6838b22bd614daf20895926af0aa0f6d
guke1024 <0124gj@gmail.com>
2022-02-03 09:42:18 +0800

    add blockchain iterator

---
+++ blockchainIterator.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	bolt "go.etcd.io/bbolt"
+	"log"
+)
+
+type BlockChainIterator struct {
+	db                 *bolt.DB
+	currentHashPointer []byte
+}
+
+func (bc *BlockChain) NewIterator() *BlockChainIterator {
+	return &BlockChainIterator{
+		bc.db,
+		bc.tail, // It initially points to the last block and changes with the next() function call
+	}
+}
+
+// Next Iterator belong to blockchain, Next() belong to Iterator
+func (it *BlockChainIterator) Next() *Block {
+	var block Block
+	_ = it.db.View(func(tx *bolt.Tx) error {
+		bucket := tx.Bucket([]byte(blockBucket))
+		if bucket == nil {
+			log.Panic("(iterator) bucket is nil")
+		}
+		blockTmp := bucket.Get(it.currentHashPointer)
+		block = Deserialize(blockTmp)
+		it.currentHashPointer = block.PrevHash // Move cursor hash left
+		return nil
+	})
+	return &block
+}
--- main.go
+++ main.go
@@ -1,14 +1,21 @@
 package main
 
+import "fmt"
+
 func main() {
 	bc := NewBlockChain()
 	bc.AddBlock("first block")
 	bc.AddBlock("second block")
-	//for i, block := range bc.blocks {
-	//	fmt.Println("Current block height: ", i)
-	//	fmt.Printf("Prev block hash: %x\n", block.PrevHash)
-	//	fmt.Printf("Current block hash: %x\n", block.Hash)
-	//	fmt.Printf("Block data: %s\n", block.Data)
-	//}
 
+	it := bc.NewIterator()
+	for {
+		block := it.Next()
+		fmt.Printf("Prev block hash: %x\n", block.PrevHash)
+		fmt.Printf("Current block hash: %x\n", block.Hash)
+		fmt.Printf("Block data: %s\n", block.Data)
+		if len(block.PrevHash) == 0 {
+			fmt.Printf("blockchain range end")
+			break
+		}
+	}
 }
